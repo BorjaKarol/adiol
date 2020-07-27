25a1530aee6399f64e25e1154bdfb2ea727315f5
BorjaKarol <borjakarol7@gmail.com>
2020-07-27 11:33:59 +0800

    db added

---
+++ adiol.sql
@@ -0,0 +1,474 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 27, 2020 at 05:33 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adiol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `CATEGORY_ID` int(11) NOT NULL,
+  `CNAME` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`CATEGORY_ID`, `CNAME`) VALUES
+(0, 'Beef'),
+(1, 'Chicken'),
+(2, 'Seafood'),
+(3, 'Pork'),
+(4, 'Processed Meat'),
+(5, 'Others');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `CUST_ID` int(11) NOT NULL,
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`CUST_ID`, `FIRST_NAME`, `LAST_NAME`, `PHONE_NUMBER`) VALUES
+(9, 'Pick-up', NULL, NULL),
+(11, 'A Walk in Customer', NULL, NULL),
+(15, 'Pick Up Garahe', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `EMPLOYEE_ID` int(11) NOT NULL,
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `GENDER` varchar(50) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL,
+  `JOB_ID` int(11) DEFAULT NULL,
+  `HIRED_DATE` varchar(50) NOT NULL,
+  `LOCATION_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EMPLOYEE_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`, `EMAIL`, `PHONE_NUMBER`, `JOB_ID`, `HIRED_DATE`, `LOCATION_ID`) VALUES
+(1, 'Adrian', 'Lester', 'Male', 'alp@adioltradingopc.com.ph', '09124033805', 1, '0000-00-00', 113),
+(5, 'Kenneth', 'Tusi', 'Male', 'kennethtusi@gmail.com', '09662560461', 3, '2020-07-25', 159),
+(6, 'Laarni', 'Ortiz', 'Female', 'larniortiz@gmail.com', '09662560462', 2, '2020-07-29', 160);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+  `JOB_ID` int(11) NOT NULL,
+  `JOB_TITLE` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`JOB_ID`, `JOB_TITLE`) VALUES
+(1, 'Owner'),
+(2, 'Sales'),
+(3, 'Storage');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `LOCATION_ID` int(11) NOT NULL,
+  `PROVINCE` varchar(100) DEFAULT NULL,
+  `CITY` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`LOCATION_ID`, `PROVINCE`, `CITY`) VALUES
+(111, 'Negros Occidental', 'Bacolod City'),
+(112, 'Negros Occidental', 'Bacolod City'),
+(113, 'Negros Occidental', 'Binalbagan'),
+(114, 'Negros Occidental', 'Himamaylan'),
+(115, 'Negros Oriental', 'Dumaguette City'),
+(116, 'Negros Occidental', 'Isabella'),
+(126, 'Negros Occidental', 'Binalbagan'),
+(130, 'Cebu', 'Bogo City'),
+(131, 'Negros Occidental', 'Himamaylan'),
+(132, 'Negros', 'Jupiter'),
+(133, 'Aincrad', 'Floor 91'),
+(134, 'negros', 'binalbagan'),
+(135, 'hehe', 'tehee'),
+(136, 'PLANET YEKOK', 'KOKEY'),
+(137, 'Camiguin', 'Catarman'),
+(138, 'Camiguin', 'Catarman'),
+(139, 'Negros Occidental', 'Binalbagan'),
+(140, 'Batangas', 'Lemery'),
+(141, 'Capiz', 'Panay'),
+(142, 'Camarines Norte', 'Labo'),
+(143, 'Camarines Norte', 'Labo'),
+(144, 'Camarines Norte', 'Labo'),
+(145, 'Camarines Norte', 'Labo'),
+(146, 'Capiz', 'Pilar'),
+(147, 'Negros Occidental', 'Moises Padilla'),
+(148, 'a', 'a'),
+(149, '1', '1'),
+(150, 'Negros Occidental', 'Himamaylan'),
+(151, 'Masbate', 'Mandaon'),
+(152, 'Aklanas', 'Madalagsasa'),
+(153, 'Batangas', 'Mabini'),
+(154, 'Bataan', 'Morong'),
+(155, 'Capiz', 'Pillar'),
+(156, 'Negros Occidental', 'Bacolod'),
+(157, 'Camarines Norte', 'Labo'),
+(158, 'Negros Occidental', 'Binalbagan'),
+(159, 'Camarines Norte', 'San Vicente'),
+(160, 'Cagayan', 'Piat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `PRODUCT_ID` int(11) NOT NULL,
+  `PRODUCT_CODE` varchar(20) NOT NULL,
+  `NAME` varchar(50) DEFAULT NULL,
+  `DESCRIPTION` varchar(250) NOT NULL,
+  `QTY_STOCK` int(50) DEFAULT NULL,
+  `ON_HAND` int(250) NOT NULL,
+  `PRICE` int(50) DEFAULT NULL,
+  `CATEGORY_ID` int(11) DEFAULT NULL,
+  `SUPPLIER_ID` int(11) DEFAULT NULL,
+  `DATE_STOCK_IN` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `SUPPLIER_ID` int(11) NOT NULL,
+  `COMPANY_NAME` varchar(50) DEFAULT NULL,
+  `LOCATION_ID` int(11) NOT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`SUPPLIER_ID`, `COMPANY_NAME`, `LOCATION_ID`, `PHONE_NUMBER`) VALUES
+(11, 'InGame Tech', 114, '09457488521'),
+(12, 'Asus', 115, '09635877412'),
+(13, 'Razer Co.', 111, '09587855685'),
+(15, 'Strategic Technology Co.', 116, '09124033805'),
+(16, 'A4tech', 155, '09775673257');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `TRANS_ID` int(50) NOT NULL,
+  `CUST_ID` int(11) DEFAULT NULL,
+  `NUMOFITEMS` varchar(250) NOT NULL,
+  `SUBTOTAL` varchar(50) NOT NULL,
+  `LESSVAT` varchar(50) NOT NULL,
+  `NETVAT` varchar(50) NOT NULL,
+  `ADDVAT` varchar(50) NOT NULL,
+  `GRANDTOTAL` varchar(250) NOT NULL,
+  `CASH` varchar(250) NOT NULL,
+  `DATE` varchar(50) NOT NULL,
+  `TRANS_D_ID` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `TYPE_ID` int(11) NOT NULL,
+  `TYPE` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`TYPE_ID`, `TYPE`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Storage');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `EMPLOYEE_ID` int(11) DEFAULT NULL,
+  `USERNAME` varchar(50) DEFAULT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
+  `TYPE_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `EMPLOYEE_ID`, `USERNAME`, `PASSWORD`, `TYPE_ID`) VALUES
+(1, 1, 'adrian.lester', '51abb9636078defbf888d8457a7c76f85c8f114c', 1),
+(11, 5, 'storage.kenneth', '51abb9636078defbf888d8457a7c76f85c8f114c', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawal_details`
+--
+
+CREATE TABLE `withdrawal_details` (
+  `ID` int(11) NOT NULL,
+  `TRANS_D_ID` varchar(250) NOT NULL,
+  `PRODUCTS` varchar(250) NOT NULL,
+  `QTY` varchar(250) NOT NULL,
+  `PRICE` varchar(250) NOT NULL,
+  `EMPLOYEE` varchar(250) NOT NULL,
+  `ROLE` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`CATEGORY_ID`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CUST_ID`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`EMPLOYEE_ID`),
+  ADD UNIQUE KEY `EMPLOYEE_ID` (`EMPLOYEE_ID`),
+  ADD UNIQUE KEY `PHONE_NUMBER` (`PHONE_NUMBER`),
+  ADD KEY `LOCATION_ID` (`LOCATION_ID`),
+  ADD KEY `JOB_ID` (`JOB_ID`);
+
+--
+-- Indexes for table `job`
+--
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`JOB_ID`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`LOCATION_ID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`PRODUCT_ID`),
+  ADD KEY `CATEGORY_ID` (`CATEGORY_ID`),
+  ADD KEY `SUPPLIER_ID` (`SUPPLIER_ID`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`SUPPLIER_ID`),
+  ADD KEY `LOCATION_ID` (`LOCATION_ID`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`TRANS_ID`),
+  ADD KEY `TRANS_DETAIL_ID` (`TRANS_D_ID`),
+  ADD KEY `CUST_ID` (`CUST_ID`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`TYPE_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `TYPE_ID` (`TYPE_ID`),
+  ADD KEY `EMPLOYEE_ID` (`EMPLOYEE_ID`);
+
+--
+-- Indexes for table `withdrawal_details`
+--
+ALTER TABLE `withdrawal_details`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `TRANS_D_ID` (`TRANS_D_ID`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `CATEGORY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `CUST_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `EMPLOYEE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `LOCATION_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=161;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `PRODUCT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `SUPPLIER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `TRANS_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `withdrawal_details`
+--
+ALTER TABLE `withdrawal_details`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`LOCATION_ID`) REFERENCES `location` (`LOCATION_ID`),
+  ADD CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`JOB_ID`) REFERENCES `job` (`JOB_ID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`SUPPLIER_ID`) REFERENCES `supplier` (`SUPPLIER_ID`);
+
+--
+-- Constraints for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD CONSTRAINT `supplier_ibfk_1` FOREIGN KEY (`LOCATION_ID`) REFERENCES `location` (`LOCATION_ID`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`CUST_ID`) REFERENCES `customer` (`CUST_ID`),
+  ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`TRANS_D_ID`) REFERENCES `withdrawal_details` (`TRANS_D_ID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_3` FOREIGN KEY (`TYPE_ID`) REFERENCES `type` (`TYPE_ID`),
+  ADD CONSTRAINT `users_ibfk_4` FOREIGN KEY (`EMPLOYEE_ID`) REFERENCES `employee` (`EMPLOYEE_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
